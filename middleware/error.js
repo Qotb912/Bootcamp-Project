4fab9f10a5774d3bcdbbaf2814ee7644c1c695a8
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-15 21:58:45 +0200

    create error file to catch unhandled error for moongos like (bad object, duplicated key, validation errors)

---
+++ middleware/error.js
@@ -0,0 +1,40 @@
+const { json } = require("express");
+const ErrorResponse = require("../utils/errorResponse");
+
+const errorHandler = (err, req, res, next)=>{
+  //TODO ={ ...err } s?
+  let error ={ ...err }
+  error.message = err.message;
+
+  //Log consle for dev
+  //def betw err.stack & err
+  console.log(err);
+  //console.log(err.code.red);
+
+  //Mongoose bad Object
+  if(err.name === 'CastError'){
+    const message = `Resource not found with id of ${err.value}`;
+    error = new ErrorResponse(message,404);
+  }
+
+  //Mongoose duplicated key
+  if(err.code === 11000){
+    const message = 'Duplicated field value entered';
+    error = new ErrorResponse(message,400);
+  }
+
+  //Mongoose validataion errors
+  if(err.name === 'ValidationError'){
+    const message = Object.values(err.errors).map(val => val.message);
+    error=new ErrorResponse(message,400);
+  }
+
+  res
+  .status(error.statusCode ||500)
+  .json({
+    success:false,
+    error:error.message || 'Server Error'
+  });
+};
+
+module.exports = errorHandler;
