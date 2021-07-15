7da5ee501408b9a5cbc785d0360f1e53efbba76d
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-15 14:41:28 +0200

    Update server.js use morgan in middleware colore for console connect db log unhandled rejection

--- server.js
+++ server.js
@@ -1,35 +1,41 @@
 const express = require('express');
 const dotenv = require('dotenv');
+//const logger =require('./middleware/logger');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
 
 //Load env var from config/config.env
-
 dotenv.config({path:'./config/config.env'});
+
+//Connect to database
+connectDB();
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-app.get('/api/v1/bootcamps',(req,res)=> {
-  //res.send({name:"Coma"}); it change content-type automtic
-  res.status(200).json({success:true , msg:'Show all bootcamps'});
-});
+//Body parser "to can read request json data in console"
+app.use(express.json());
 
-app.post('/api/v1/bootcamps',(req,res)=> {
-  res.status(201).json({success:true , msg:'Create new bootcamp'});
-});
+//Dev logging Middleware
+//app.use(logger);
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
 
-app.get('/api/v1/bootcamps/:id',(req,res)=> {
-  res.status(201).json({success:true , msg:`Show bootcamp ${req.params.id}`});
-});
-
-
-app.put('/api/v1/bootcamps/:id',(req,res)=> {
-  res.status(201).json({success:true , msg:`Update bootcamp ${req.params.id}`});
-});
-
-app.delete('/api/v1/bootcamps/:id',(req,res)=> {
-  res.status(201).json({success:true , msg:`Delete bootcamp ${req.params.id}`});
-});
-
+//Mount routers
+app.use('/api/v1/bootcamps',bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+const server =app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold));
+
+process.on('unhandledRejection',(err,promise)=>{
+  //TODO S?
+  console.log(`Error:${err.message}`.red);
+  //Close server & exit process
+  server.close(()=>process.exit(1));
+});
