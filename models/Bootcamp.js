8ca75a3484631f34836e634d96330316d56ae85b
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-16 04:16:27 +0200

    Create seeder file to read json data and save it in DB or delete them from it

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -42,12 +42,12 @@ const BootcampSchema = mongoose.Schema({
     type: {
       type: String,
       enum: ['Point'],
-      required: true
+      //required: true
     },
     coordinates: {
       type: [Number],
-      required: true,
       index:'2dsphere'
+      //required: true,
     },
     formattedAddress:String,
     street:String,
---
+++ seeder.js
@@ -0,0 +1,57 @@
+//TODO fs s?
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+//Lod env var 
+dotenv.config({path:'./config/config.env'});
+
+//Load Model
+const Bootcamp =require('./models/Bootcamp');
+const connectDB = require('./config/db');
+const { json } = require('express');
+
+//connect to DB
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser:true,
+  useCreateIndex:true,
+  useFindAndModify:false,
+  useUnifiedTopology:true
+});
+
+//Read JSON files
+const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+
+//Import into DB
+const importData = async ()=>{
+  try {
+    await Bootcamp.create(bootcamps);
+    console.log('Data Impoted... '.green.inverse);
+    // process.exit();  and it's kind to s?
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
+//Delete into DB
+const deleteData = async ()=>{
+  try {
+    await Bootcamp.deleteMany();
+    console.log('Data Destroyed... '.red.inverse);
+    // process.exit();  and it's kind to s?
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+//to run this we write in terminal node seeder -i --> -i = index 2 in argv so it will run importData() fun
+// else node seeder -d it will run  deleteDate() fun
+if(process.argv[2] === '-i'){
+  importData();
+}else if(process.argv[2] === '-d'){
+  deleteData();
+}
