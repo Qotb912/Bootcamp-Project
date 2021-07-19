add554276b3005265e587bdf63e2f629681aa2be
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-19 05:40:05 +0200

    Create model , controller and route files for courses

---
+++ routes/courses.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse
+ }=require('../controllers/courses');
+
+ 
+  // import models  
+  const Course = require('../models/Course');
+  // import advancedResults
+  const advancedResults = require('../middleware/advancedResults');
+
+
+const router = express.Router({mergeParams:true});
+
+router
+ .route('/')
+ .get(advancedResults(Course,{
+  path:'bootcamp',
+  select:'name description'
+}) ,getCourses)
+ .post(addCourse);
+
+ router
+ .route('/:id')
+ .get(getCourse)
+ .put(updateCourse)
+ .delete(deleteCourse);
+
+
+ module.exports =router;
