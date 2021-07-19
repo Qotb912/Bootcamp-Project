5fe59006dec9bb4632c2842672df0cccf0cc1819
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-19 05:36:00 +0200

    Update bootcamp route with Re-route , advancedResults middleware , getBootcampsInRadius router and bootcampUploadPhoto router

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -4,19 +4,38 @@ const {
   createBootcamp,
   getBootcamp,
   updateBootcamp,
-  deleteBootcamp }=require('../controllers/bootcamps');
+  deleteBootcamp,
+  getBootcampsInRadius,
+  bootcampUploadPhoto }=require('../controllers/bootcamps');
 
+  // import models  
+  const Bootcamp = require('../models/Bootcamp');
+  // import advancedResults
+  const advancedResults = require('../middleware/advancedResults');
+
+//Include other ressourse routers
+const courseRouter = require('./courses');
 
 const router = express.Router();
 
+//Re-route into other ressourse router
+router
+  .use('/:bootcampId/courses',courseRouter);
+
 router
   .route('/')
-  .get(getBootcamps)
+  .get(advancedResults(Bootcamp,'courses'), getBootcamps)
   .post(createBootcamp);
 router
   .route('/:id')
   .get(getBootcamp)
   .put(updateBootcamp)
   .delete(deleteBootcamp);
+router
+  .route('/radius/:zipcode/:distance')
+  .get(getBootcampsInRadius);
+router
+  .route('/:id/photo')
+  .put(bootcampUploadPhoto);
 
 module.exports = router;
