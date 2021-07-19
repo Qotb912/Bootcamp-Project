fa53540749f5606bd9b08cefd3900e81f32d520d
Qotb912 <mahmoudqotb912@gmail.com>
2021-07-19 05:22:21 +0200

    Update seeder to import and delete courses from database

--- seeder.js
+++ seeder.js
@@ -9,6 +9,8 @@ dotenv.config({path:'./config/config.env'});
 
 //Load Model
 const Bootcamp =require('./models/Bootcamp');
+const Course = require('./models/Course');
+
 const connectDB = require('./config/db');
 const { json } = require('express');
 
@@ -22,11 +24,14 @@ mongoose.connect(process.env.MONGO_URI,{
 
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
+const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
+
 
 //Import into DB
 const importData = async ()=>{
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('Data Impoted... '.green.inverse);
     // process.exit();  and it's kind to s?
     process.exit();
@@ -40,6 +45,7 @@ const importData = async ()=>{
 const deleteData = async ()=>{
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('Data Destroyed... '.red.inverse);
     // process.exit();  and it's kind to s?
     process.exit();
