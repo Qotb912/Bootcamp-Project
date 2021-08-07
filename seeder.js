0f42b52f5ffe724300eb0c27e2156f8c64367929
Qotb912 <mahmoudqotb912@gmail.com>
2021-08-07 17:16:16 +0200

    Update seeder with user and review

--- seeder.js
+++ seeder.js
@@ -10,6 +10,8 @@ dotenv.config({path:'./config/config.env'});
 //Load Model
 const Bootcamp =require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
+const Review = require('./models/Review');
 
 const connectDB = require('./config/db');
 const { json } = require('express');
@@ -25,6 +27,8 @@ mongoose.connect(process.env.MONGO_URI,{
 //Read JSON files
 const bootcamps = JSON.parse(fs.readFileSync(`${__dirname}/_data/bootcamps.json`,'utf-8'));
 const courses = JSON.parse(fs.readFileSync(`${__dirname}/_data/courses.json`,'utf-8'));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/_data/users.json`,'utf-8'));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/_data/reviews.json`,'utf-8'));
 
 
 //Import into DB
@@ -32,6 +36,8 @@ const importData = async ()=>{
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
+    await Review.create(reviews);
     console.log('Data Impoted... '.green.inverse);
     // process.exit();  and it's kind to s?
     process.exit();
@@ -46,6 +52,8 @@ const deleteData = async ()=>{
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('Data Destroyed... '.red.inverse);
     // process.exit();  and it's kind to s?
     process.exit();
